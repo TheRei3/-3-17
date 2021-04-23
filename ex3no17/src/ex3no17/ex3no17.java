96ef9ccca376f00dcda7e0ea90c8a634da0c6d5f
admin <admin@DESKTOP-8L2IG92>
2021-04-24 06:11:41 +0900

    ex3no17

---
+++ ex3no17/src/ex3no17/ex3no17.java
@@ -0,0 +1,11 @@
+package ex3no17;
+
+public class ex3no17 {  //106p
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = (int)(pi * 1000 + 0.5) / 1000.0;
+		
+		System.out.println(shortPi);
+	}
+
+}
